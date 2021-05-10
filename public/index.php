b66fce55ec6d21c10ba5f2d2c648529bd26406f2
Erra <erich.hobe@mail.ee>
2021-05-10 09:39:08 +0300

    ok

--- public/index.php
+++ public/index.php
@@ -53,3 +53,17 @@ $response = tap($kernel->handle(
 ))->send();
 
 $kernel->terminate($request, $response);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <li
+    <title>Document</title>
+</head>
+<body>
+    <h1>Arki ajad</h1>
+
+</body>
+</html>
